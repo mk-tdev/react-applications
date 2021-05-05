f13d25d84c0e7a88ae76cbc5cf13768bcc9e4d88
Muthu Kumar <mk-dev@outlook.com>
2021-05-05 23:15:50 +0800

    webpack-updates

---
+++ expensify/readme.md
@@ -0,0 +1,15 @@
+# Git Commands
+
+git init
+git status
+git add
+git commit
+git log
+git push
+
+# dependencies
+
+## npm
+
+npm install --save moment react react-dates react-dom react-redux react-router-dom redux uuid
+npm install -D @babel/cli @babel/core @babel/plugin-proposal-class-properties @babel/preset-env @babel/preset-react babel-loader css-loader node-sass redux-devtools sass-loader style-loader webpack webpack-cli webpack-dev-server
--- expensify/webpack.config.js
+++ expensify/webpack.config.js
@@ -1,33 +1,53 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/app.js",
-  output: {
-    path: path.resolve(__dirname, "public/"),
-    filename: "app.bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = (env) => {
+  console.log("env: ", env);
+  const modeConfig = env.production ? "production" : "development";
+  const sourceMapConfig = env.production ? "source-map" : "inline-source-map";
+
+  return {
+    mode: modeConfig,
+    entry: "./src/app.js",
+    output: {
+      path: path.resolve(__dirname, "public/"),
+      filename: "app.bundle.js",
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: "styles.css" })],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    compress: true,
-    open: true,
-    port: 7070,
-    historyApiFallback: true,
-  },
+        {
+          test: /\.s?css$/i,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            {
+              loader: "css-loader",
+              options: { sourceMap: true },
+            },
+            {
+              loader: "sass-loader",
+              options: { sourceMap: true },
+            },
+          ],
+
+          // use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    devtool: sourceMapConfig,
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      compress: true,
+      open: true,
+      port: 7070,
+      historyApiFallback: true,
+    },
+  };
 };
