516e7d40ce462ea744cfaf346d99c905df6e42c6
Muthu Kumar <mk-dev@outlook.com>
2021-05-20 23:29:52 +0800

    tests-ui-interation

--- expensify/src/app/tests/components/ExpenseForm.test.js
+++ expensify/src/app/tests/components/ExpenseForm.test.js
@@ -21,3 +21,54 @@ test("should render ExpenseForm with expense data", () => {
   const wrapper = shallow(<ExpenseForm expense={expense} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test("should render error when submitting with no data", () => {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("form").simulate("submit", { preventDefault: () => {} });
+
+  expect(wrapper.state("errorText").length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should update state when description changes", () => {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("input")
+    .at(0)
+    .simulate("change", { target: { value: "desc" } });
+
+  expect(wrapper.state("description")).toBe("desc");
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should update state when note textarea changes", () => {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("textarea")
+    .simulate("change", { target: { value: "this is description" } });
+
+  expect(wrapper.state("note")).toBe("this is description");
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should update state when valid amount provided", () => {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("input")
+    .at(1)
+    .simulate("change", { target: { value: "10.80" } });
+
+  expect(wrapper.state("amount")).toBe("10.80");
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should not update state when in-valid amount provided", () => {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("input")
+    .at(1)
+    .simulate("change", { target: { value: "10.880" } });
+
+  expect(wrapper.state("amount")).toBe("");
+  expect(wrapper).toMatchSnapshot();
+});
